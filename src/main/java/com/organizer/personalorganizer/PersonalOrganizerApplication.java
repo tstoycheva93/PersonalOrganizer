0550e648d7c98eb306cbed0349ae28b47aeca482
Teodora Stoycheva <s22621693@onlineedu.tu-varna.bg>
2025-02-24 14:44:13 +0200

    Initial commit

---
+++ src/main/java/com/organizer/personalorganizer/PersonalOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.organizer.personalorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalOrganizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/organizer/personalorganizer/PersonalOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organizer.personalorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
