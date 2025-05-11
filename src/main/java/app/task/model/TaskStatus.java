4e98dabeaf1b12db6b6976d01fd866cbdb8eed2f
Diyan Dimitrov <dimitrovdian330@gmail.com>
2025-05-11 21:06:05 +0300

    fixed some bugs (version 2.3)

--- src/main/java/app/task/model/TaskStatus.java
+++ src/main/java/app/task/model/TaskStatus.java
@@ -1,5 +1,18 @@
 package app.task.model;
 
 public enum TaskStatus {
-    NOT_STARTED, IN_PROGRESS, COMPLETED, PASSED
+    NOT_STARTED("TO DO"),
+    IN_PROGRESS("DOING"),
+    COMPLETED("DONE"),
+    PASSED("PASSED");
+
+    private final String label;
+
+    TaskStatus(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
